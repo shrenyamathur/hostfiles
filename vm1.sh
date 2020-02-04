eba6a0aa151d9e561f2fd694d12e3239d7e35315
shrenyamathur <60392198+shrenyamathur@users.noreply.github.com>
2020-02-04 17:47:42 +0530

    Add files via upload

---
+++ vm1.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+mkdir uploadfiles
+cd uploadfiles
+Git clone https://github.com/shrenyamathur/hostfiles
+cd hostfiles
+sudo ufw allow 5000/tcp
+sudo apt-get update
+sudo apt-get install python-pip
+pip install azure. storage
+pip install flask
+Python azure103_project.py
