7e81a1b28f60ea4fb5869de3453f3162b7bd52a0
shrenyamathur <60392198+shrenyamathur@users.noreply.github.com>
2020-02-05 16:09:20 +0530

    Update vm1.sh

--- vm1.sh
+++ vm1.sh
@@ -6,5 +6,6 @@ sudo ufw allow 5000/tcp
 sudo apt-get update
 sudo apt-get install python-pip
 pip install azure.storage
+pip install configparser
 pip install flask
 cd hostfiles && python azure103_project.py
