a57ce1ad8a43c61efe8eb766f398ac7f6f992c01
shrenyamathur <60392198+shrenyamathur@users.noreply.github.com>
2020-02-05 15:57:53 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 [DEFAULT]
 # Account name
-account = 'storage3121'
+account = storage3121
 # Azure Storage account access key 
-key ='ejqJbOmEyrGlqJ/vQCK2AXiMvQBLvbOaG85450SxYRw8wCp9HOccHjMAXVPH4ZjOccr8yoUz6DUbhBhL1MbSug=='
+key = ejqJbOmEyrGlqJ/vQCK2AXiMvQBLvbOaG85450SxYRw8wCp9HOccHjMAXVPH4ZjOccr8yoUz6DUbhBhL1MbSug==
 # Container name
-container ='container312'
+container =container312
